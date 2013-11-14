d91348ef14a722328fd213111f16738cfaf13cfb
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-11-13 22:40:22 -0700

    beginning converter

---
+++ data/greek/mounce_dictionary/convert_txt_to_json.rb
@@ -0,0 +1,6 @@
+# GK G1592 | S G1458   ἐγκαλέω   enkaleō   7x
+# <def>can be followed by a dative, to bring a charge against, accuse; to institute judicial proceedings, Acts 19:38, 40; 23:28, 29; 26:2, 7; Rom. 8:33 </def>→ accuse.
+
+File.open(file, "r") do |file|
+
+end
