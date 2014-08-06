39749f263cf8fd3673d46af4c1cedd8def6ec599
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-08-06 16:49:45 -0600

    Fallback to index.html; npm scripts update

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ var express = require('express')
 var app = express()
 const PORT = process.env.NODE_ENV === 'production' ? 80 : 4200
 app.use(express.static(__dirname + '/dist'))
+app.get('*', function(req, res){
+  res.sendfile(__dirname + '/dist/index.html');
+});
 console.log('Listening to port ' + PORT)
 app.listen(PORT)
