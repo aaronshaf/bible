21a6a81b18146fd4b716b0ba643fda168efdda5c
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-08-09 10:29:57 -0600

    Use direct s3 bucket URL in local development

--- app/config/env.js
+++ app/config/env.js
@@ -1,3 +1,10 @@
+var API_HOST
+if(location.hostname.indexOf('local') > -1) {
+  API_HOST = 'https://s3.amazonaws.com/api.bible.theopedia.com/'
+} else {
+  API_HOST = 'http://api.bible.theopedia.com/'
+}
+
 module.exports = {
-  API_HOST: 'http://api.bible.theopedia.com/' //window.localStorage.API_HOST || 
+  API_HOST: API_HOST
 }
