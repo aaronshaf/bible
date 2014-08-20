4d8561c58f28844cc491a22bab349209d19dbbce
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-08-20 05:01:05 -0600

    Serve up JSON as UTF-8

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
 const PORT = process.env.NODE_ENV === 'production' ? 80 : 4200
+app.use(function(req, res, next) {
+  if (/.*\.json/.test(req.path)) {
+    res.set({ 'content-type': 'application/json; charset=utf-8' })
+  }
+  next()
+})
 app.use(express.static(__dirname + '/dist'))
 app.use('/api',express.static(__dirname + '/node_modules/aaronshaf-bible-data'))
 app.get('*', function(req, res){
